9d8788948233d3b2fe1575fafb0b50e5a53b7148
yamatatsu <the.kinnikuman@gmail.com>
2018-07-03 03:42:49 +0900

    provide store

--- modules/phenyl-todo-app/src/app/index.js
+++ modules/phenyl-todo-app/src/app/index.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { StyleSheet, Text, View, Button } from "react-native";
 import { createStackNavigator } from "react-navigation";
+import { createStore } from "phenyl-todo-core";
+import NavigationService from "./navigation-service";
 
 const First = props => {
   const { navigation } = props;
@@ -28,8 +31,18 @@ const RootNavigator = createStackNavigator({
   },
 });
 
+const store = createStore();
+
 const App = () => {
-  return <RootNavigator />;
+  return (
+    <Provider store={store}>
+      <RootNavigator
+        ref={navigatorRef => {
+          NavigationService.setTopLevelNavigator(navigatorRef);
+        }}
+      />
+    </Provider>
+  );
 };
 export default App;
 
